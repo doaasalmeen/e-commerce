6b4b09f2686935ff7f2b2c8c0ac20b8b7ef3baae
DoaaSalmen <doaasalmen258@gmail.com>
2025-10-29 20:01:15 +0100

    Add Helmet and Winston for security and logging

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
+import helmet from "helmet"; 
+import morgan from 'morgan'; 
+
 
 const app = express();
+app.use(helmet()); 
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true})); 
+
+app.use(morgan('combined', 
+{stream:{write:(message)=>logger.info(message.trim())}})); 
 
 app.get("/", (req, res) => {
+  logger.info('hello from logger'); 
   res.status(200).send("hello from app.js");
 });
 
